7f66052679904382011bae5ea05e95590e1d7bf8
German Velasco <germsvel@gmail.com>
2018-05-14 09:44:28 -0400

    Only run mix sync --no-halt if outside iex
    When in `iex`, we do not want to run `mix sync` with a `--no-halt` flag.
    We update the `mix sync` task to account for that.

--- apps/client/lib/mix/tasks/sync.ex
+++ apps/client/lib/mix/tasks/sync.ex
@@ -3,6 +3,19 @@ defmodule Mix.Tasks.Sync do
 
   def run(args) do
     Logger.info("Starting mana node")
-    Mix.Tasks.Run.run(args ++ ["--no-halt"])
+
+    Mix.Tasks.Run.run(run_args() ++ args)
+  end
+
+  def run_args do
+    if iex_running?() do
+      []
+    else
+      ["--no-halt"]
+    end
+  end
+
+  defp iex_running? do
+    Code.ensure_loaded?(IEx) and IEx.started?()
   end
 end
